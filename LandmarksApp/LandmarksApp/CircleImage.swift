4f93798eca618ebc08b1333ae58d715828b7561c
Chorrs <Pechorskaya.K8@gmail.com>
2024-02-11 18:57:16 +0200

    The view that shows a landmark’s details has been built.

---
+++ LandmarksApp/LandmarksApp/CircleImage.swift
@@ -0,0 +1,24 @@
+//
+//  CircleImage.swift
+//  LandmarksApp
+//
+//  Created by Chorrs on 11.02.24.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay {
+                Circle()
+                    .stroke(.white, lineWidth: 4)
+            }
+            .shadow(radius: 7)
+    }
+}
+
+//#Preview {
+//    CircleImage()
+//}
